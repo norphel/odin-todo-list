b69c6b126c26c92b68c9cbff7b8d2cef5b321062
norphel <norphel317@gmail.com>
2023-05-04 23:15:11 +0530

    add project dialogue box

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Webpack Check 1 2 3...");
+
+
+// import styles
+
+import 'normalize.css';
+import './styles/styles.css';
+
+
+const addProjectBtn = document.getElementById('add-project-btn');
+const dialogueBox = document.querySelector('.dialogue-box');
+const overlay = document.querySelector('.overlay');
+const cancelBtn = dialogueBox.querySelector('.btn-cancel');
+
+addProjectBtn.addEventListener('click', () => {
+  dialogueBox.classList.add('active');
+  overlay.classList.add('active');
+});
+
+cancelBtn.addEventListener('click', () => {
+  dialogueBox.classList.remove('active');
+  overlay.classList.remove('active');
+});
